0416946b6bf2a0a06277e4db71941b8fb86ed8c2
azarry <azarryonex@gmail.com>
2021-02-25 19:40:01 +0300

    Матросов Евгений. 2ую задачу так и не смог решит застопорился в том моменте когда полученные числа перевел в листы, но ни  как не мог понять как получить их суммы не записывая результат в новый список.

---
+++ lesson1 -task2.py
@@ -0,0 +1,16 @@
+my_list=[]
+n=0
+for i in range(1,10,2):
+    my_list.append(i**3) #заполняем список нечетными элементами возведенные в куб
+    for t in range(0,len(my_list)):
+        q=list(str(my_list[t])) #каждый элемент списка my_list (q) переводим в список
+        # for y in range(0,len(list_my_list)):
+        #     answer=int(list_my_list[t][y])
+        # for y in range (0, len(list_my_list)):
+        #     q=int(list_my_list[0][y])
+        #     print(q)
+        #     for x in range(list_my_list[0],)
+print(q)
+print(t)
+    #for my_list in range ():
+print (my_list)
